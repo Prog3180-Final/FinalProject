bf7f00fd9836d873435cc3dae23ac30e5b8a6312
TheEndurance <rawa.jalal@gmail.com>
2018-04-22 20:04:00 -0400

    trust unsecure certificates lol

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-certificates.Certificates",
+    "file": "plugins/cordova-plugin-certificates/www/certificate.js",
+    "pluginId": "cordova-plugin-certificates",
+    "clobbers": [
+      "cordova.plugins.certificates"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-certificates": "0.6.4"
 };
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-certificates/www/certificate.js",
+        "id": "cordova-plugin-certificates.Certificates",
+        "pluginId": "cordova-plugin-certificates",
+        "clobbers": [
+            "cordova.plugins.certificates"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-certificates": "0.6.4"
 }
 // BOTTOM OF METADATA
 });
